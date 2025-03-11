77d558cdbaf92f1108066313b6f0eca1854084de
Vishal-46 <vishalmurugan006@gmail.com>
2025-03-11 07:01:48 +0530

    commit

--- JS.js
+++ JS.js
@@ -14,3 +14,22 @@ function Exfunc (){
 //this instead of that 
 
 <button onClick = {() => {console.log("return...")}}
+
+//Ternary operators
+
+let age = 19;
+let name = "Vi";
+if (age > 10){
+    name = "Vi"
+}else{
+    name = "Ui"
+}
+//as
+let age = 19;
+let name = age > 10 && "Vi";
+//or 
+let age = 19;
+let name = age > 10 || "Vi";
+//as
+let age = 19;
+let name = age > 10 ? "Vi" : "Ui";
